b3e132a60ad500d34e23cf75e0ee7351b1697a4d
pincman <pincman@qq.com>
2022-07-02 15:42:46 +0800

    第一次提交

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# react-admin
+# Toome Admin
+
+
+
+*>* *目前版本是使用React17写的，React18版本正在开发中，升级完毕正式上线，目前版本最好学习使用，不保证BUG和问题处理*
+
+
+
+一款支持KeepLive,标签拖动，Echarts，多种Loading,嵌套配置式路由等功能
+
+
+
+主要使用以下技术
+
+- 路由[React Router v6](https://reactrouter.com/)
+- 状态管理[Zustand](https://github.com/pmndrs/zustand)
+- 数据获取[Axios](https://github.com/axios/axios)+[Swr](https://swr.vercel.app/zh-CN)
+- 图标[Echarts](https://echarts.apache.org/zh/index.html)
+- 组件库[Antd](https://ant.design/index-cn),React18版本会换成[Arco](https://arco.design/)
+- 动画[React-Spring](https://react-spring.dev/)
+- 拖动[React-dnd](https://react-dnd.github.io/react-dnd/)
+- 图标[iconify](https://iconify.design/)
+- 其它如[ahooks](https://ahooks.gitee.io/zh-CN),[immer](https://github.com/immerjs/immer),[react-loadingg](https://github.com/Summer-andy/react-loading),[react-use-measure](https://github.com/pmndrs/react-use-measure)等
+
+
+
+支持网站[平克小站](https://pincman.com),实现过程学习[react18最佳实践](https://v.pincman.com/courses/66.html)
--- src/views/dashboard/monitor/index.blade.tsx
+++ src/views/dashboard/monitor/index.blade.tsx
@@ -1,29 +1,92 @@
-import { Col, Input, Row } from 'antd';
+import { Button, Card, Col, Input, Row } from 'antd';
 import { useCallback, useState } from 'react';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
 
-import { DndContainer } from './dnd';
+import { ServerApp, ServerInfo } from './components';
 
-const MonitorDashboard = () => {
-    const [ddd, setDdd] = useState('monitor');
+const analysisTabs = [
+    {
+        key: 'network',
+        tab: '网络',
+    },
+    {
+        key: 'load',
+        tab: '负载',
+    },
+    {
+        key: 'io',
+        tab: '读写io',
+    },
+];
+const analysisPanel = {
+    network: <p>load content</p>,
+    // network: <NetworkPanel />,
+    load: <p>load content</p>,
+    io: <p>sdsd</p>,
+};
+const Dashboard = () => {
+    const [analysis, setAnalysis] = useState('network');
+    const changeAnalysis = useCallback((tab: string) => {
+        setAnalysis(tab);
+    }, []);
+    const [ddd, setDdd] = useState('ddd');
     const changeDdd = useCallback(
         (e: React.ChangeEvent<HTMLInputElement>) => setDdd(e.target.value),
         [],
     );
     return (
-        <DndProvider backend={HTML5Backend}>
-            <div className=" md:mx-auto w-6/12">
-                <Row gutter={[8, 16]}>
-                    <Col span={24}>
-                        <Input value={ddd} onChange={changeDdd} />
-                    </Col>
-                    <Col span={24}>
-                        <DndContainer />
-                    </Col>
-                </Row>
-            </div>
-        </DndProvider>
+        <div className="md:container md:mx-auto">
+            <Row gutter={[8, 16]}>
+                <Col span={24}>
+                    <Input value={ddd} onChange={changeDdd} />
+                </Col>
+                <Col span={24}>
+                    <Card
+                        title="状态"
+                        bordered={false}
+                        className="w-full"
+                        bodyStyle={{
+                            paddingTop: '10px',
+                            paddingBottom: '10px',
+                        }}
+                        extra={
+                            <>
+                                <Button type="primary" className="mr-1">
+                                    登录
+                                </Button>
+                                <Button className="mr-1">关机</Button>
+                                <Button className="mr-1">重启</Button>
+                                <Button>重置密码</Button>
+                            </>
+                        }
+                    >
+                        <Row>
+                            <Col span={4}>{/* <CpuMonitor /> */}</Col>
+                            <Col span={4}>{/* <MemoryMonitor /> */}</Col>
+                            <Col span={4}>{/* <CacheBufferMonitor /> */}</Col>
+                            <Col span={4}>{/* <DiskMonitor /> */}</Col>
+                        </Row>
+                    </Card>
+                </Col>
+                <Col span={24} xl={8}>
+                    <Card className="w-full">
+                        <ServerInfo />
+                    </Card>
+                </Col>
+                <Col span={24} xl={16}>
+                    <Card
+                        style={{ width: '100%' }}
+                        tabList={analysisTabs}
+                        activeTabKey={analysis}
+                        onTabChange={changeAnalysis}
+                    >
+                        {analysisPanel[analysis]}
+                    </Card>
+                </Col>
+                <Col span={24}>
+                    <ServerApp />
+                </Col>
+            </Row>
+        </div>
     );
 };
-export default MonitorDashboard;
+export default Dashboard;
